421290f3d3b50ba9416fae40f8cfed0655717fd1
Gavyn Holt <gavynholt@gmail.com>
2022-10-15 22:13:40 -0400

    fix: styles and api endpoint

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://localhost:8080',
+  apiUrl: 'https://gj-wedding-rsvp.herokuapp.com',
   googleMapsApi: 'AIzaSyDSzMXqEeexpopRHdtU9L9gpr8LWQQ55J4',
 };
